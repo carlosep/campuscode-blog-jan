28460349551b302f1941d65022c19ffc38e0c061
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-04-23 09:45:35 -0300

    Fixed test for homepage

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-  factory :post do
-    title 'John Doe goes to mars'
-    body 'Mars is a wonderful place for vacation.'
+  factory :post do |post|
+    post.title {"Title of post #{rand(1000).to_s}.."}
+    post.body {"Body of post #{(0...50).map { ('a'..'z').to_a[rand(26)] }.join} "}
   end
 end
--- spec/features/visitor/visitor_visits_homepage_spec.rb
+++ spec/features/visitor/visitor_visits_homepage_spec.rb
@@ -6,21 +6,45 @@ feature 'Visitor visits homepage' do
     expect(page).to have_content 'Welcome to Blog-Jan take a seat.'
   end
 
-  scenario 'and sees all posts' do
-    post_1 = create(:post)
-    post_2 = create(:post, title: 'Title for Post2', body: 'Body for Post2.')
-    post_3 = create(:post, title: 'Title for Post3', body: 'Body for Post3.')
-    post_4 = create(:post, title: 'Title for Post4', body: 'Body for Post4.')
+  scenario 'and sees last ten posts' do
+    post_1  = create(:post)
+    post_2  = create(:post)
+    post_3  = create(:post)
+    post_4  = create(:post)
+    post_5  = create(:post)
+    post_6  = create(:post)
+    post_7  = create(:post)
+    post_8  = create(:post)
+    post_9  = create(:post)
+    post_10 = create(:post)
+    post_11 = create(:post)
+    post_12 = create(:post)
 
     visit root_path
 
-    expect(page).to have_content post_1.title
-    expect(page).to have_content post_1.body
-    expect(page).to have_content post_2.title
-    expect(page).to have_content post_2.body
+    expect(page).not_to have_content post_1.title
+    expect(page).not_to have_content post_2.body
+    expect(page).not_to have_content post_2.title
+    expect(page).not_to have_content post_2.body
     expect(page).to have_content post_3.title
     expect(page).to have_content post_3.body
     expect(page).to have_content post_4.title
     expect(page).to have_content post_4.body
+    expect(page).to have_content post_5.title
+    expect(page).to have_content post_5.body
+    expect(page).to have_content post_6.title
+    expect(page).to have_content post_6.body
+    expect(page).to have_content post_7.title
+    expect(page).to have_content post_7.body
+    expect(page).to have_content post_8.title
+    expect(page).to have_content post_8.body
+    expect(page).to have_content post_9.title
+    expect(page).to have_content post_9.body
+    expect(page).to have_content post_10.title
+    expect(page).to have_content post_10.body
+    expect(page).to have_content post_11.title
+    expect(page).to have_content post_11.body
+    expect(page).to have_content post_12.title
+    expect(page).to have_content post_12.body
   end
 end
