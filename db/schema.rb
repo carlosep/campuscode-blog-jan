3f14182bfaa6a46058f7a05e554c19f259ad04f9
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-04-25 21:14:36 -0300

    Added subtitle to posts on DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305121707) do
+ActiveRecord::Schema.define(version: 20160426000603) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20160305121707) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "subtitle",   default: "-"
   end
 
   create_table "users", force: :cascade do |t|
